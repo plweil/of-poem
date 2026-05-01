69de48734ee21901cddc93f2621de4fa91bcf617
Peter Weil <plweil@wisc.edu>
2026-05-01 15:38:08 -0500

    content edit

--- app/Views/about.php
+++ app/Views/about.php
@@ -8,17 +8,17 @@ require BASE_PATH . '/app/Views/partials/header.php';
 <main class="content">
     <h1>About this Site: Rights and Permissions</h1>
   <p>The poems presented here are the work of their respective authors. Many first appeared in <cite>Elizabeth</cite> or under the imprint of either
-    <cite>The Elizabeth Press</cite> or <cite>James L. Weil, Publisher</cite>; others were published elsewhere and reprinted in or by one of the above.</p>
+    The Elizabeth Press or James L. Weil, Publisher. Others were reprinted in <cite>Elizabeth</cite> from other small presses, poetry reviews, etc.</p>
 
   <p>This site is a non-commercial project intended to make a small selection of these works available in a new context. Every effort has been made to identify authors and original sources accurately.</p>
 
   <p>If you are a rights holder and have questions or concerns about the inclusion of any material, please <a href="mailto:info@jlwpub.com">contact us</a> and we will respond promptly.</p>
 
 <h2>Artwork</h2>
-  <p>The cover device (“Fool”) for <cite>Elizabeth</cite>, which also served as the publisher’s mark for <cite>The Elizabeth Press</cite> was by Paul Lett.</p>
+  <p>The cover device (“Fool”) for <cite>Elizabeth</cite>, which also served as the publisher’s mark for The Elizabeth Press, was by Paul Lett.</p>
   <p>
-    The publisher’s mark for <cite>James L. Weil, Publisher</cite> was created by
-    <span>Jacques Hnizdovsky</span>.
+    The publisher’s mark for James L. Weil, Publisher was created by
+    <a href="https://hnizdovsky.gallery/">Jacques Hnizdovsky</a>.
   </p>
 </main>
 
