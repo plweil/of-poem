128ada289d73ce400c9a5ca0bb3468522735469f
Peter Weil <plweil@wisc.edu>
2026-05-04 07:08:58 -0500

    remove unneeded files and dirs

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import * as Vue from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
-import FeaturedPoem from './components/FeaturedPoem.js';
-
-const { createApp } = Vue;
-const app = createApp({});
-app.component('FeaturedPoem', FeaturedPoem);
-app.mount('#app');
