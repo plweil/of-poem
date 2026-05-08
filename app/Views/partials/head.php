a02dc219b93e1589be8338bd71c6dbd9cfd685e2
Peter Weil <plweil@wisc.edu>
2026-05-07 20:35:23 -0500

    add Garamond Google font

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -5,6 +5,10 @@
   <title><?= htmlspecialchars($title ?? 'Of Poem: James L. Weil and the poetry he loved') ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;1,400&display=swap" rel="stylesheet">
+
   <link rel="stylesheet" href="<?= BASE_URL ?>/css/site.css">
 </head>
 <body>
