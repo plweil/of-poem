b448031c42780179edf29322518f18c7d3a6b6f2
Peter Weil <plweil@wisc.edu>
2026-05-27 21:08:22 -0500

    tweak page title

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <title><?= htmlspecialchars(!empty($pageTitle)
         ? "$pageTitle | Of Poem — James L. Weil"
-        : "Of Poem: James L. Weil") ?>
+        : "Of Poem — James L. Weil") ?>
   </title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
